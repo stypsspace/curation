92cbc5a3463658af061fe3f1f8ce262abdf7f0f1
Mitchell Lemisa <139455432+stypsspace@users.noreply.github.com>
2023-07-19 20:38:49 +0200

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,14 +3,14 @@ import React from 'react'
 
 const PageNotFound = () => {
   return (
-    <section className='section'>
-      <div className='container'>
-        <h1 className='text-3xl font-bold'>404</h1>
+    <section className='404-wrap'>
+      <div className='404-container'>
+        <h1 className='404-title'>404</h1>
         <h2>That page cannot be found!</h2>
-        <div className='mt-3'>
+        <div className='404-link-wrap'>
           <Link
             href='/'
-            className='mt-2 bg-emerald-400 text-white px-3 py-1 rounded-md'
+            className='404-link'
           >
             Go Home
           </Link>
