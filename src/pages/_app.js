3973147c80704f73388a0dc866bc28a58daa4a8a
Mitchell Lemisa <139455432+stypsspace@users.noreply.github.com>
2023-07-20 18:19:06 +0200

    Update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import Layout from '@/components/layout';
 import '@/styles/globals.css';
 import '@/styles/custom.css';
 import '@/styles/animation.css'; 
+import '@/styles/responsive.css'; 
 import 'typeface-inter';
 
 export default function App({ Component, pageProps }) {
