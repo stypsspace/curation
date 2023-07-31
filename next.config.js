33b602a7f681a6b1c93377385d7b1e39e168d88d
Mitchell Lemisa <139455432+stypsspace@users.noreply.github.com>
2023-07-31 09:49:48 +0200

    Update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const path = require('path');
+
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },
+};
