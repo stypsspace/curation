4a6046a866dc17ed0e5131ed58630c4060f6a077
Mitchell Lemisa <139455432+stypsspace@users.noreply.github.com>
2023-08-01 18:07:54 +0200

    Update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,23 +1,22 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          {/* Add your favicon link here */}
-          <link rel="icon" href="/favicon_2.ico" type="image/x-icon" />
-          {/* Other meta tags, stylesheets, or scripts can be added here */}
-          <script defer data-domain="styps.space" src="https://plausible.io/js/script.js"></script>
+render() {
+return (
+<Html>
+<Head>
+{/* Add your favicon link here */}
+<link rel="icon" href="/favicon_2.ico" type="image/x-icon" />
+{/* Other meta tags, stylesheets, or scripts can be added here */}
 
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+</Head>
+<body>
+<Main />
+<NextScript />
+</body>
+</Html>
+);
+}
 }
 
 export default MyDocument;
