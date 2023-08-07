6bf3a33d9c37d2b876fc18df8946b23e46473334
Mitchell Lemisa <139455432+stypsspace@users.noreply.github.com>
2023-08-07 14:21:53 +0200

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,13 @@ return (
 <Head>
 {/* Add your favicon link here */}
 <link rel="icon" href="/favicon_2.ico" type="image/x-icon" />
+
+{/* Klaviyo */}
+<script
+type="text/javascript"
+src="//static.klaviyo.com/onsite/js/klaviyo.js?company_id=U6mWb3"
+></script>
+
 {/* Other meta tags, stylesheets, or scripts can be added here */}
 
 </Head>
