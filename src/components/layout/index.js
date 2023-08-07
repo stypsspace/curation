fecde03fc794beef2c02dd7eb8c94f22dde115ba
Mitchell Lemisa <139455432+stypsspace@users.noreply.github.com>
2023-08-07 14:47:06 +0200

    Update

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -42,7 +42,27 @@ Menu
 
 <footer className='site-footer'>
 <div className='site-footer-content'>
+
+<div>
+<div className='styps-logo'>
+<Link href="/" className="site-header-logo">
+  <img src="/logo.png" alt="Logo" width={400} height={400} />
+</Link>
+</div>
+</div>
+
+<div>
+<div class="klaviyo-form-UeNcKv"></div>
+</div>
+
+<div>
 <p>©2023 Styps Ltd</p>
+</div>
+
+
+
+
+
 </div>
 </footer>
 
