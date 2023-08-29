7b7158b1c89e862bc477eaecddd6b90705f77d93
Mitchell Lemisa <139455432+stypsspace@users.noreply.github.com>
2023-08-29 13:01:19 +0200

    Update

--- src/lib/contentful.js
+++ src/lib/contentful.js
@@ -5,4 +5,15 @@ const client = createClient({
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 });
 
+// Function to fetch "Advert" entries
+export async function getAdvertEntries() {
+  try {
+    const response = await client.getEntries({ content_type: 'advert' });
+    return response.items;
+  } catch (error) {
+    console.error('Error fetching Advert entries:', error);
+    return []; // Return an empty array in case of an error
+  }
+}
+
 export default client;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-import client from 'src/lib/contentful';
+//import client from 'src/lib/contentful'; // Import only the client object
+import client, { getAdvertEntries } from 'src/lib/contentful';
 import Image from 'next/image';
 import Head from 'next/head';
 
@@ -14,7 +15,7 @@ const ContentfulImage = (props) => {
 };
 
 
-const Posts = ({ posts }) => {
+const Posts = ({ posts, adverts }) => {
   const [selectedCategory, setSelectedCategory] = useState('');
   const [isFilterContainerVisible, setIsFilterContainerVisible] = useState(false);
 
@@ -77,19 +78,103 @@ const Posts = ({ posts }) => {
         <p>Curated websites for inspiration and promotion of good design</p>
       </div>
 
-      <div>
-        {/* Display the page view count for the index page */}
-        
-      </div>
-
-      <ul className='category-container'>
-        {filteredPosts.map((post) => {
-          const { title, slug, coverImage, video, date, author, externalUrl } = post.fields;
-          return (
-            <li className='' key={slug}>
-              
-              <div className='post-image'>
+     {/* Display "Post" and "Advert" entries */}
+<ul className='category-container'>
+  {filteredPosts.concat(adverts)
+  
+  .sort((entry1, entry2) => {
+    // Determine the correct date field for each entry type
+    const date1 = entry1.fields.date || entry1.fields.startDate;
+    const date2 = entry2.fields.date || entry2.fields.startDate;
+    // Sort entries in descending order based on their publication date
+    return new Date(date2) - new Date(date1);
+  })
+    .map((entry) => {
+      if (entry.fields.category) {
+        // This is a "Post" entry
+        const { title, slug, coverImage, video, date, author, externalUrl } = entry.fields;
+        return (
+          <li className='' key={slug}>
+            <div className='post-image'>
+              <Link href={`/posts/${slug}`} aria-label={title}>
+                {coverImage && coverImage.fields && coverImage.fields.file && (
+                  <ContentfulImage
+                    alt={`Cover Image for ${title}`}
+                    src={coverImage.fields.file.url}
+                    width={400}
+                    height={300}
+                    loading='lazy'
+                  />
+                )}
+              </Link>
+            </div>
+            {video && (
+              <div className='post-video'>
                 <Link href={`/posts/${slug}`} aria-label={title}>
+                  <div className='video-wrapper'>
+                    <video
+                      id={`video-${slug}`}
+                      className='video-player'
+                      src={video.fields.file.url}
+                      width={400}
+                      height={300}
+                      autoPlay
+                      loop
+                      preload='metadata' // Preload only the metadata for faster loading
+                      muted // Mute the video to prevent audio playback on page load
+                      playsInline // Add playsInline attribute to prevent picture-in-picture on mobile
+                    ></video>
+                  </div>
+                </Link>
+              </div>
+            )}
+           <div className='post-date'>
+            {date && (
+            <time dateTime={new Date(date).toISOString().slice(0, 10)}>
+              {new Date(date).toLocaleDateString('en-US', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+              })}
+            </time>
+            )}
+            </div>
+
+          
+            <div className='author-name-wrap'>
+            {author && author.fields && author.fields.picture && author.fields.picture.fields && (
+            <div className='author-name-image'>
+            <ContentfulImage
+            src={author.fields.picture.fields.file.url}
+            layout='fixed'
+            width={40}
+            height={40}
+            loading='lazy'
+            className='author-name'
+            alt={author.fields.name}
+            />
+            </div>
+            )}
+            {author && author.fields && author.fields.name && (
+            <div className='author-name'>{author.fields.name}</div>
+            )}
+            </div>
+
+            <div className='post-header'>
+              <h3>{title}</h3>
+            </div>
+          </li>
+
+        );
+      } else {
+        // This is an "Advert" entry
+        const { title, slug, coverImage, externalUrl, startDate, endDate } = entry.fields;
+        return (
+          <li className='' key={slug}>
+            {externalUrl && (
+              <a href={externalUrl} target="_blank" rel="noopener noreferrer">
+                <button className='advert-button'>Ad</button>
+                <div className='advert-image'>
                   {coverImage && coverImage.fields && coverImage.fields.file && (
                     <ContentfulImage
                       alt={`Cover Image for ${title}`}
@@ -99,65 +184,36 @@ const Posts = ({ posts }) => {
                       loading='lazy'
                     />
                   )}
-                </Link>
-              </div>
-
-              {video && (
-                <div className='post-video'>
-                  <Link href={`/posts/${slug}`} aria-label={title}>
-                    <div className='video-wrapper'>
-                      <video
-                        id={`video-${slug}`}
-                        className='video-player'
-                        src={video.fields.file.url}
-                        width={400}
-                        height={300}
-                        autoPlay
-                        loop
-                        preload='metadata' // Preload only the metadata for faster loading
-                        muted // Mute the video to prevent audio playback on page load
-                        playsInline // Add playsInline attribute to prevent picture-in-picture on mobile
-                      ></video>
-                    </div>
-                  </Link>
                 </div>
-              )}
+              
+              </a>
+            )}
+            <div className='post-date'>
+            {entry.fields.date && (
+            <time dateTime={new Date(entry.fields.date).toISOString().slice(0, 10)}>
+            {new Date(entry.fields.date).toLocaleDateString('en-US', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+            })}
+            </time>
+            )}
+            </div>
 
-              <div className='post-date'>
-                <time dateTime={new Date(date).toISOString().slice(0, 10)}>
-                  {new Date(date).toLocaleDateString('en-US', {
-                    day: '2-digit',
-                    month: '2-digit',
-                    year: 'numeric',
-                  })}
-                </time>
-              </div>
+            <div className='advert-title'>
+              <h3>{title}</h3>
+            </div>
+           
+          </li>
 
-              <div className='author-name-wrap'>
-                <div className='author-name-image'>
-                  <ContentfulImage
-                    src={author.fields.picture.fields.file.url}
-                    layout='fixed'
-                    width={40}
-                    height={40}
-                    loading='lazy'
-                    className='author-name'
-                    alt={author.fields.name}
-                  />
-                </div>
-                <div className='author-name'>{author.fields.name}</div>
-              </div>
-
-
-              <div className='post-header'>
-                <h3>{title}</h3>
-              </div>
+        );
+      }
+    })}
+</ul>
 
 
-            </li>
-          );
-        })}
-      </ul>
+
+
 
 
       <div className='open-filter-container'></div>
@@ -205,16 +261,23 @@ const Posts = ({ posts }) => {
 };
 
 export async function getStaticProps() {
-  const response = await client.getEntries({ content_type: 'post' });
+  const postResponse = await client.getEntries({ content_type: 'post' });
+  const advertResponse = await getAdvertEntries(); // Use the function we defined
 
-  const posts = response.items.map((post) => ({
+  const posts = postResponse.items.map((post) => ({
     sys: post.sys,
     fields: post.fields,
+  }));
+
+  const adverts = advertResponse.map((advert) => ({
+    sys: advert.sys,
+    fields: advert.fields,
   }));
 
   return {
     props: {
       posts,
+      adverts,
     },
   };
 }
